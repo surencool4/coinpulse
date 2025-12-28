b7684be2ed6f4c4077f31d6f2323644fc86cf674
Surendra Budhathoki <surendra@pahadi.net>
2025-12-28 20:40:01 +0545

    Add API integration from coingecko

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,18 @@
-import React from 'react'
-import Image from "next/image";
-import DataTable from "@/components/DataTable";
-import Link from "next/link";
-import {cn} from "@/lib/utils";
-import {TrendingDown, TrendingUp} from "lucide-react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import {Suspense} from "react";
 
-const columns: DataTableColumn<TrendingCoin>[] = [
-    {
-        header: 'Name',
-        cellClassName: 'name-cell',
-        cell: (coin) => {
-            const item = coin.item;
-            return (
-                <Link href={`/coins/${item.id}`}>
-                    <Image src={item.large} alt={item.name} width={36} height={36}/>
-                    <p>{item.name}</p>
-                </Link>
-            )
-        }
-    },
-    {
-        header: '24h Change',
-        cellClassName: 'name-cell',
-        cell: (coin) => {
-            const item = coin.item;
-            const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
-            return (
-                <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
-                    <p>
-                        {isTrendingUp ? (
-                            <TrendingUp width={16} height={16} />
-                            )
-                        :( <TrendingDown width={16} height={16} />)}
-                        {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
-                    </p>
-                </div>
-            )
-        }
-    },
-    {
-        header: 'Price',
-        cellClassName: 'price-cell',
-        cell: (coin) => coin.item.data.price
-    },
-]
+const Page = async () => {
 
-const Page = () => {
     return (
         <main className="main-container">
             <section className="home-grid">
-                <div id="coin-overview">
-                    <div className="header pt-2">
-                        <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56} />
-                        <div className="info">
-                            <p>Bitcoin / BTC</p>
-                            <h1>$87,113.00</h1>
-                        </div>
-                    </div>
-                </div>
-
-                <p>Trending Coins</p>
-                <DataTable
-                    data={[]}
-                    columns={[]}
-                />
+                <Suspense fallback={<div>Loading Overview...</div>}>
+                    <CoinOverview />
+                </Suspense>
+                <Suspense fallback={<div>Loading Trending...</div>}>
+                    <TrendingCoins />
+                </Suspense>
             </section>
             <section className="w-full mt-7 space-y-4">
                 <p>Categories</p>
--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images:{
+      domains: ['coin-images.coingecko.com'],
       remotePatterns:[
           {
               protocol: "https",
               hostname: "assets.coingecko.com",
+          },
+          {
+              protocol: "https",
+              hostname: "coin-images.coingecko.com",
           }
       ]
   }
